2513faaa1ae25f3338a5d6ee031739743180e59e
IsaelGSF <isaelgsfaria@gmail.com>
2025-04-07 02:31:50 -0300

    Add projectile animation

--- microjogos/grandeisa/bullet-hell/cenas/scripts/projectile.gd
+++ microjogos/grandeisa/bullet-hell/cenas/scripts/projectile.gd
@@ -7,14 +7,25 @@ var main
 
 var moving = false
 
+@onready var animator = $AnimationPlayer
+
 func _ready():
-	look_at(player.position)
+	animator.play("start")
 	
 func _physics_process(delta: float) -> void:
 	if moving:
 		position += transform.x * SPEED * delta
+	if animator.current_animation == "start":
+		look_at(player.position)
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		main.register_lose()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "start":
+		moving = true
+		animator.play("default")
+		
